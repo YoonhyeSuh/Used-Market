5dd150b3c8884651adb13e3d47868afc5407d00a
JHR <115974764+haeri-J@users.noreply.github.com>
2023-10-23 15:38:56 +0900

    main_menu.xml 파일 생성 및 구현 완료, nav.xml 생성

--- Boogimarket/app/src/main/java/com/example/boogimarket/MainActivity.kt
+++ Boogimarket/app/src/main/java/com/example/boogimarket/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.boogimarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+        }
+        return true
+
     }
 }
