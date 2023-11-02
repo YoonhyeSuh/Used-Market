3f9db68e6336248f04fbdb5575602c4ffb7270a4
JHR <115974764+haeri-J@users.noreply.github.com>
2023-11-02 19:16:55 +0900

    data class 추가
    chat Data와 Message Data를 받아올 자료형 클래스를 추가로 만들어줌

--- Boogimarket/app/src/main/java/com/example/boogimarket/User.kt
+++ Boogimarket/app/src/main/java/com/example/boogimarket/User.kt
@@ -6,3 +6,16 @@ data class User (
     val userId: String,
     val password: String
 )
+
+data class ChatData(
+    val id: String?,
+    val writer: User,
+    val contact: User,
+    val messages: List<MessageData>
+)
+
+data class MessageData(
+    val content: String,
+    val createdAt: Long,
+    val from: Int
+)
