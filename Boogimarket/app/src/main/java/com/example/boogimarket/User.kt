47775772faf65dd7561235b39494e48a113120e0
JHR <115974764+haeri-J@users.noreply.github.com>
2023-11-02 22:32:58 +0900

    User.kt 수정
    자료형 클래스 업데이트

--- Boogimarket/app/src/main/java/com/example/boogimarket/User.kt
+++ Boogimarket/app/src/main/java/com/example/boogimarket/User.kt
@@ -6,16 +6,3 @@ data class User (
     val userId: String,
     val password: String
 )
-
-data class ChatData(
-    val id: String?,
-    val writer: User,
-    val contact: User,
-    val messages: List<MessageData>
-)
-
-data class MessageData(
-    val content: String,
-    val createdAt: Long,
-    val from: Int
-)
