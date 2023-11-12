d1034c92a9b39faae5de1fb1f546208fde1922eb
YoonhyeSuh <106311524+YoonhyeSuh@users.noreply.github.com>
2023-11-12 19:23:39 +0900

    로그인, 회원가입 액티비티 수정
    실시간 db 저장할 수 있게 수정
    
    UserAdapter에서 userId 오류가 떠서 일단 User 데이터 클래스의 uId를 userId로 바꿈 나중에 원인 알아보고 바꿔야할 것 같음
    
    그리고 회원정보 등록, 로그인 다 가능  하지만 회원가입 하고 파이어베이스에서 realtime database를 들어가면 user가 안나옴

--- Boogimarket/app/src/main/java/com/example/boogimarket/RegisterActivity.kt
+++ Boogimarket/app/src/main/java/com/example/boogimarket/RegisterActivity.kt
@@ -7,12 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.boogimarket.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityRegisterBinding
     lateinit var mAuth: FirebaseAuth
+
+    //실시간 데이터 베이스
+    private lateinit var mDbRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
@@ -21,24 +27,28 @@ class RegisterActivity : AppCompatActivity() {
         //인증 초기화
         mAuth = Firebase.auth
 
+        //db 초기화
+        mDbRef = Firebase.database.reference
+
         //회원가입 버튼 회원가입 기능 구현
         binding.signUpBtn.setOnClickListener {
 
             //trim() 공백 제거
             //이메일, 비밀번호, 이름, 생일 저장
             val email = binding.email.text.toString().trim()
-            val pw = binding.pw.text.toString().trim()
+            val password = binding.password.text.toString().trim()
             val name = binding.name.text.toString().trim()
             val birth = binding.birth.text.toString().trim()
 
             //signUp 함수 호출 회원 정보 등록
             //이메일과 비밀번호 인자로 넘김
-            signUp(email, pw, name, birth)
+            signUp(email, password, name, birth)
         }
     }
 
-    private fun signUp(email: String, pw: String, name: String, birth: String){
-        mAuth.createUserWithEmailAndPassword(email, pw)
+    //회원가입
+    private fun signUp(email: String, password: String, name: String, birth: String){
+        mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     //성공시 메인액티비티 이동
@@ -47,10 +57,18 @@ class RegisterActivity : AppCompatActivity() {
                     Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
                     val intent: Intent = Intent(this@RegisterActivity, LoginActivity::class.java)
                     startActivity(intent)
+
+                    //사용자 정보 저장
+                    addUserToDatabase(name, birth, email, mAuth.currentUser?.uid!!)
+
                 } else {
                     //실패시 실행
                     Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun addUserToDatabase(name: String, birth: String, email: String, userId: String ){
+        mDbRef.child("user").child(userId).setValue(User(name, birth, email, userId))
     }
 }
