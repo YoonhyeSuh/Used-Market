9187a5584dceea050e6e611acb46d599959e83bd
YoonhyeSuh <106311524+YoonhyeSuh@users.noreply.github.com>
2023-11-12 17:20:01 +0900

    로그인, 회원가입 xml과 액티비티 수정
    로그인, 회원가입 xml과 액티비티 수정

--- Boogimarket/app/src/main/java/com/example/boogimarket/LoginActivity.kt
+++ Boogimarket/app/src/main/java/com/example/boogimarket/LoginActivity.kt
@@ -1,13 +1,26 @@
 package com.example.boogimarket
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.boogimarket.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //회원가입 버튼 이벤트
+        //뷰 바인딩 이용해서 아이디로 이벤트 만들기
+        binding.registerBtn.setOnClickListener{
+            //액티비티의 이동은 intent
+            val intent: Intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
