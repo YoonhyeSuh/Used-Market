db97693cade166ce00c7d006d5ba0567ed55f96e
YoonhyeSuh <106311524+YoonhyeSuh@users.noreply.github.com>
2023-11-20 22:07:52 +0900

    MyPage 액티비티 수정
    로그아웃과 액션바 코드 수정해봤지만 여전히 안됨,, 내일 다시,,

--- Boogimarket/app/src/main/java/com/example/boogimarket/MyPageActivity.kt
+++ Boogimarket/app/src/main/java/com/example/boogimarket/MyPageActivity.kt
@@ -4,15 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.boogimarket.databinding.ActivityMypageBinding
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-
+//1120 로그아웃과 액션바 코드 고쳐봤지만 안된다^^
 class MyPageActivity : AppCompatActivity() {
     lateinit var binding: ActivityMypageBinding
+
+    //아이콘 버튼 클릭시 로그아웃
+    fun onIconClick() {
+        Firebase.auth.signOut()
+        val intent = Intent(this@MyPageActivity, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,21 +26,16 @@ class MyPageActivity : AppCompatActivity() {
 
         //회원가입 정보 읽고 쓰기
         //하..^^
+        //1120 코드 고쳐봤지만 로그아웃 액션바 안됨,,
 
-        //메뉴바에서 navigation 연결을 아직 안해 확인 불가
-        //1119 확인해본 결과 로그아웃 버튼 누르면 앱이 종료됨
-        //아이콘 버튼 클릭시 로그아웃
         binding.logoutBtn.setOnClickListener {
-            Firebase.auth.signOut()
-            val intent = Intent(this@MyPageActivity, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+            onIconClick()
         }
 
-        //1119 확인해본 결과 액션바 안나타남
         //액션바 이름 설정
         supportActionBar?.title = "MyPageActivity"
         //뒤로 가기 버튼
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
