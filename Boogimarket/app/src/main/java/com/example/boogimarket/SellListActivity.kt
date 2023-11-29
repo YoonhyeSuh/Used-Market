7a189d58f995e5164c870e2f278cd2420cb667c0
YoonhyeSuh <106311524+YoonhyeSuh@users.noreply.github.com>
2023-11-29 21:37:46 +0900

    feat. 내가 쓴 글 불러오기 추가
    내가 쓴 글 불러오기에 필요한 xml 파일과 activity 파일 생성

---
+++ Boogimarket/app/src/main/java/com/example/boogimarket/SellListActivity.kt
@@ -0,0 +1,15 @@
+package com.example.boogimarket
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SellListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_myselllist)
+
+
+    }
+}
